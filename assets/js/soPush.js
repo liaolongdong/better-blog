f4ee4cdc193ce73b4527680bba423a210d068b98
liaolongdong <924902324@qq.com>
2019-01-17 16:13:00 +0800

    增加360代码推送以及压缩图片

---
+++ assets/js/soPush.js
@@ -0,0 +1,4 @@
+(function(){
+    var src = (document.location.protocol == "http:") ? "http://js.passport.qihucdn.com/11.0.1.js?268d507166f60ddd94f7798251aa2452":"https://jspassport.ssl.qhimg.com/11.0.1.js?268d507166f60ddd94f7798251aa2452";
+    document.write('<script src="' + src + '" id="sozz"><\/script>');
+})();
