9e68070bdd93e11f3bc5a9e8766d41e1abbf28ea
liaolongdong <924902324@qq.com>
2019-02-15 16:41:20 +0800

    修改阿里云部署shell脚本

--- deploy-ali.sh
+++ deploy-ali.sh
@@ -4,6 +4,11 @@ function git.branch {
 }
 # 获取当前分支
 git.branch
+# git fetch 指令是下载远程仓库最新内容，不做合并
+git fetch --all
+# git reset 指令把HEAD指向master最新版本
+# git reset --hard origin/master
+git reset --hard origin/${br/* /}
 # 拉取当前分支信息
 git pull origin ${br/* /}:${br/* /}
 # 打包部署
