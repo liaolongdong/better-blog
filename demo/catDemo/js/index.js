fe9fab9950bbf35fa39d87b79f60e279aece93ab
liaolongdong <924902324@qq.com>
2019-07-17 15:01:56 +0800

    增加花脸猫

---
+++ demo/catDemo/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    // 设备宽度小于1024px和关于我的页面不显示猫
+    let path = window.location.pathname;
+    let width = window.innerWidth;
+    if (path.indexOf('/about.html') !== -1 || width < 1024) {
+        $('.mao_box').hide();
+    }
+})
