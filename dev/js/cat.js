b9df93f247ef617faa59ab90673ed511dbda5a7d
liaolongdong <924902324@qq.com>
2019-07-23 09:23:01 +0800

    小屏幕电脑不显示花脸猫

--- dev/js/cat.js
+++ dev/js/cat.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
-    // 设备宽度小于1024px和关于我的页面不显示猫
+    // 设备宽度小于1500px和关于我的页面不显示猫
     let path = window.location.pathname;
     let width = window.innerWidth;
-    if (path.indexOf('/about.html') !== -1 || width < 1024) {
+    console.log('可视宽度：', width);
+    if (path.indexOf('/about.html') !== -1 || width < 1500) {
         $('.mao_box').hide();
     }
 })
