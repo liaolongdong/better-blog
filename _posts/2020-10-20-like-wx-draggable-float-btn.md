a4f4b426677524f820982cd633284d9251dc3c1e
liaolongdong <924902324@qq.com>
2020-11-07 16:31:30 +0800

    H5实现类似微信可拖拽左右吸边浮动按钮博客

--- _posts/2020-10-20-like-wx-draggable-float-btn.md
+++ _posts/2020-10-20-like-wx-draggable-float-btn.md
@@ -1,3 +1,36 @@
+---
+layout: post
+title: H5实现类似微信可拖拽左右吸边浮动按钮
+subtitle: H5实现类似微信可拖拽左右吸边浮动按钮
+date: 2020-10-20
+categories: 技术 前端小技巧 实现可拖拽悬浮按钮
+# cover: /assets/img/postCover/gulp_cover.png
+tags: JavaScript vue 实现可拖拽悬浮按钮
+---
+
+# H5实现类似微信可拖拽左右吸边浮动按钮
+
+## 背景
+
+之前项目需求产品经理要求实现一个`可以实时拖拽的按钮`，刚好我们移动端H5用的调试工具vconsole也有类似功能，于是研究了一下vconsole里面具体实现的源码，参考其中代码自己也实现了一个, [点击查看demo效果](https://liaolongdong.com/demo/realtimeDraggable/index.html)
+
+然后在做另外一个需求的时候产品经理无意中看到我手机微信的那个可以拖拽并且可以自动左右吸附的浮窗按钮，说这个效果不错，就按照微信的这个效果来，于是在原来的那个基础上有实现了一个`类似微信可拖拽左右吸边浮动按钮`，[点击查看demo效果](https://liaolongdong.com/demo/likeWxFloatDragBtnDemo/index.html)
+
+## 技术实现
+
+其实实现这个功能所使用到的技术点并不难，主要是要处理边界情况和各种细节
+
+实现实时拖拽功能技术点：
+
+- 移动端： 监听`touchstart`、`touchmove`、`touchend`事件
+- PC端： 监听`mousedown`、`mousemove`、`mouseup`事件
+- 以及一些dom操作的api，获取节点的位置等
+
+## 实现源码
+
+废话不多说，直接上源码，这里只放js代码，要看html/css/js，直接可以在demo页面，按`F12`查看完整内容
+
+```js
 const app = new Vue({
   el: "#app",
   data: {
@@ -94,3 +127,11 @@ const app = new Vue({
     },
   },
 });
+```
+
+## demo效果地址
+
+- [H5实现类似微信可拖拽左右吸边浮动按钮](https://liaolongdong.com/demo/likeWxFloatDragBtnDemo/index.html)
+- [H5实现类似于vconsole的实时拖拽功能](https://liaolongdong.com/demo/realtimeDraggable/index.html)
+
+
--- demo/realtimeDraggable/js/realtime-draggable.js
+++ demo/realtimeDraggable/js/realtime-draggable.js
@@ -1,59 +1,66 @@
 /*
  * @Author: liaolongdong
  * @Date: 2020-04-26 17:53:26
- * @LastEditTime: 2020-04-26 18:44:24
+ * @LastEditTime: 2020-11-07 16:03:37
  * @LastEditors: liaolongdong
  * @Description: 移动端实现类似于vconsole的实时拖拽功能
  */
 (function (window, document) {
-    const draggable_params = {
-        x: 20, // right
-        y: 100, // bottom
-        startX: 0,
-        startY: 0,
-        endX: 0,
-        endY: 0
-    }
+  const draggable_params = {
+    x: 20, // right
+    y: 100, // bottom 该值必须是初始定位样式值的倍数(比如使用750px设计稿的需要设置初始值为设计稿的一半)，否则首次拖拽可能会错位
+    startX: 0,
+    startY: 0,
+    endX: 0,
+    endY: 0,
+  };
 
-    const rtDraggable = {
-        init (node) {
-            node && node.addEventListener('touchstart', (e) => {
-                draggable_params.startX = e.touches[0].pageX;
-                draggable_params.startY = e.touches[0].pageY;
-            });
-            node && node.addEventListener('touchend', (e) => {
-                draggable_params.x = draggable_params.endX;
-                draggable_params.y = draggable_params.endY;
-                draggable_params.startX = 0;
-                draggable_params.startY = 0;
-            });
-            node && node.addEventListener('touchmove', (e) => {
-                if (e.touches.length > 0) {
-                    let offsetX = e.touches[0].pageX - draggable_params.startX,
-                        offsetY = e.touches[0].pageY - draggable_params.startY;
-                    let x = draggable_params.x - offsetX,
-                        y = draggable_params.y - offsetY;
-                    // check edge
-                    if (x + node.offsetWidth > document.documentElement.offsetWidth) {
-                        x = document.documentElement.offsetWidth - node.offsetWidth;
-                    }
-                    if (y + node.offsetHeight > document.documentElement.offsetHeight) {
-                        y = document.documentElement.offsetHeight - node.offsetHeight;
-                    }
-                    if (x < 0) { x = 0; }
-                    if (y < 0) { y = 0; }
-                    // 如果节点不是fixed定位，则设置成fixed定位
-                    if (node.style.position !== 'fixed') {
-                        node.style.position = 'fixed';
-                    }
-                    node.style.right = x + 'px';
-                    node.style.bottom = y + 'px';
-                    draggable_params.endX = x;
-                    draggable_params.endY = y;
-                    e.preventDefault();
-                }
-            });
-        },
-    }
-    window.rtDraggable = rtDraggable;
+  const rtDraggable = {
+    init(node) {
+      node &&
+        node.addEventListener("touchstart", (e) => {
+          draggable_params.startX = e.touches[0].pageX;
+          draggable_params.startY = e.touches[0].pageY;
+        });
+      node &&
+        node.addEventListener("touchend", (e) => {
+          draggable_params.x = draggable_params.endX;
+          draggable_params.y = draggable_params.endY;
+          draggable_params.startX = 0;
+          draggable_params.startY = 0;
+        });
+      node &&
+        node.addEventListener("touchmove", (e) => {
+          if (e.touches.length > 0) {
+            let offsetX = e.touches[0].pageX - draggable_params.startX,
+              offsetY = e.touches[0].pageY - draggable_params.startY;
+            let x = draggable_params.x - offsetX,
+              y = draggable_params.y - offsetY;
+            // check edge
+            if (x + node.offsetWidth > document.documentElement.offsetWidth) {
+              x = document.documentElement.offsetWidth - node.offsetWidth;
+            }
+            if (y + node.offsetHeight > document.documentElement.offsetHeight) {
+              y = document.documentElement.offsetHeight - node.offsetHeight;
+            }
+            if (x < 0) {
+              x = 0;
+            }
+            if (y < 0) {
+              y = 0;
+            }
+            // 如果节点不是fixed定位，则设置成fixed定位
+            if (node.style.position !== "fixed") {
+              node.style.position = "fixed";
+            }
+            node.style.right = x + "px";
+            node.style.bottom = y + "px";
+            draggable_params.endX = x;
+            draggable_params.endY = y;
+            e.preventDefault();
+          }
+        });
+    },
+  };
+  window.rtDraggable = rtDraggable;
 })(window, document);
